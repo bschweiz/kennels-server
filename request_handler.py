2977af9d3ebc2a5f49bf95f30e60533de2146f42
Benjamin Schweizer <bschweiz@gmail.com>
2021-01-12 15:02:22 -0600

    added working empoyees module, tested in Postman

--- request_handler.py
+++ request_handler.py
@@ -2,6 +2,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal
 from locations import get_all_locations, get_single_location
+from employees import get_all_employees, get_single_employee
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -71,6 +72,12 @@ class HandleRequests(BaseHTTPRequestHandler):
                 response = f'{get_single_location(id)}'
             else:
                 response = f'{get_all_locations()}'
+        #Then we check if it's employees
+        elif resource == "employees":
+            if id is not None:
+                response = f'{get_single_employee(id)}'
+            else:
+                response = f'{get_all_employees()}'
 
     # This weird code sends a response back to the client
         self.wfile.write(f"{response}".encode())
