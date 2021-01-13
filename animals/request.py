6fbc6872a4be62be3e2d7c85c09e5c2538ea7772
Benjamin Schweizer <bschweiz@gmail.com>
2021-01-13 15:07:00 -0600

    core edit/put function of ch6 finished, now need to apply to locs, custs, and emps

--- animals/request.py
+++ animals/request.py
@@ -4,27 +4,33 @@ ANIMALS = [
         "name": "Snickers",
         "species": "Dog",
         "locationId": 1,
-        "customerId": 4
+        "customerId": 4,
+        "status": "Admitted"
     },
     {
         "id": 2,
         "name": "Gypsy",
         "species": "Dog",
         "locationId": 1,
-        "customerId": 2
+        "customerId": 2,
+        "status": "Admitted"
     },
     {
         "id": 3,
         "name": "Blue",
         "species": "Cat",
         "locationId": 2,
-        "customerId": 1
+        "customerId": 1,
+        "status": "Admitted"
     }
 ]
+
 
 def get_all_animals():
     return ANIMALS
 # Function with a single parameter
+
+
 def get_single_animal(id):
     # Variable to hold the found animal, if it exists
     requested_animal = None
@@ -39,6 +45,7 @@ def get_single_animal(id):
 
     return requested_animal
 
+
 def create_animal(animal):
     # get id value of the LAST ANIMAL IN THE LISIIIIIISSSST
     max_id = ANIMALS[-1]["id"]
@@ -48,14 +55,15 @@ def create_animal(animal):
     animal["id"] = new_id
     # add the animal dict. to the pre-existing ANIMALS list
     ANIMALS.append(animal)
-    # return the dictionary JUST CREATED, but now with added & appropriate 'id' property 
+    # return the dictionary JUST CREATED, but now with added & appropriate 'id' property
     return animal
-    
+
+
 def delete_animal(id):
     # initial -1 value for animal index, in case one isn't found
     animal_index = -1
     # iterate the ANIMALS list, but use !!!enumerate()!!! so that
-    #you can access the index value of each item
+    # you can access the index value of each item
     for index, animal in enumerate(ANIMALS):
         if animal["id"] == id:
             # Found the animal. store the current index.
@@ -63,3 +71,12 @@ def delete_animal(id):
     # if the animal was found, use !!!pop(int)!!! to remove it from the list
     if animal_index >= 0:
         ANIMALS.pop(animal_index)
+
+def update_animal(id, new_animal):
+    # iterate the ANIMALS list, but use !!!!enumerate() so that
+    #you can access the index value of each item.
+    for index, animal in enumerate(ANIMALS):
+        if animal["id"] == id:
+            # found the proper ANIMAL now update it
+            ANIMALS[index] = new_animal
+            break
--- request_handler.py
+++ request_handler.py
@@ -1,7 +1,7 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from locations import get_all_locations, get_single_location, create_location, delete_location
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
@@ -154,8 +154,16 @@ class HandleRequests(BaseHTTPRequestHandler):
         # It handles any PUT request.
 
     def do_PUT(self):
-        self.do_POST()
-
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+        # parse the URL 
+        (resource, id) = self.parse_url(self.path)
+        # delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+        self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
